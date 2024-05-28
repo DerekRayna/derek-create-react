1352b42227b2870be737f3e6982263fd2def978a
DerekRayna <987408090@qq.com>
2024-05-28 23:32:07 +0800

    feat: 使用husky在commit之前自动校验代码

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,6 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 
-const a: string = 1
-console.log(a)
-
 const root = document.getElementById('root')
 if (root) {
   createRoot(root).render(<App />)
